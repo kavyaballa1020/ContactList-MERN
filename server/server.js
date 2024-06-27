a797cf648346013ea0165e2c7c59497cd6e3b04c
Balla Kavya <ballakavya594@gmail.com>
2024-06-27 20:18:28 +0530

    "Added dotenv config, updated MongoDB connection to use environment variable, and added logging for MongoDB connection"

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const dotenv = require('dotenv');
 
 const contactRoutes = require('./routes/contactRoutes');
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,7 +14,9 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/contacts', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
 
 app.use('/contacts', contactRoutes);
 
