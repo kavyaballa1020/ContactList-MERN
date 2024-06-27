8801a421ae759bf8b482104341af02f613b55008
Balla Kavya <ballakavya594@gmail.com>
2024-06-27 18:20:31 +0530

    "Deleted file: client/src/components/services/contactService.js"

---
+++ client/src/components/ContactList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ContactList = ({ contacts, updateContact, deleteContact }) => {
+  return (
+    <div>
+      <h2>Contact List</h2>
+      <ul>
+        {contacts.map(contact => (
+          <li key={contact._id}>
+            {contact.name} - {contact.email} - {contact.phone}
+            <button onClick={() => deleteContact(contact._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ContactList;
